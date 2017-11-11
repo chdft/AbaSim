80fa4c43f3a1fc2431fe76365c8b718a65c67b32
ChDF T <chdft-dev@outlook.de>
2017-11-11 18:09:41 +0100

    add j instruction

---
+++ AbaSim.Core/Virtualization/Abacus16/Operations/SimpleJumOperationUnit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbaSim.Core.Virtualization.Abacus16.Operations
+{
+	class SimpleJumOperationUnit:JumpOperationUnit
+	{
+		protected override void InternalExecute()
+		{
+			ProgramCounterChange = SignedConstant;
+		}
+	}
+}
