68d3b43b54d99bd61a1616a9e52e692382454e49
ChDF T <chdft-dev@outlook.de>
2017-12-16 22:46:26 +0100

    add Cache concept

---
+++ AbaSim.Core/Virtualization/MemoryCache.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbaSim.Core.Virtualization.Abacus16
+{
+	public abstract class MemoryCache<Word> : IMemoryProvider<Word> where Word : IWord
+	{
+		public MemoryCache(IMemoryProvider<Word> backingMemoryProvider)
+		{
+			BackingMemoryProvider = backingMemoryProvider;
+		}
+
+		public int Size
+		{
+			get { return BackingMemoryProvider.Size; }
+		}
+
+		public abstract Word this[int index]
+		{
+			get;
+			set;
+		}
+
+		protected IMemoryProvider<Word> BackingMemoryProvider { get; private set; }
+
+		public void Flush()
+		{
+			FlushToBackingMemory();
+			BackingMemoryProvider.Flush();
+		}
+
+		protected abstract void FlushToBackingMemory();
+	}
+}
